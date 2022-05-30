75cbae6f80756df35092be8a3c278f66502e4e9e
chehaisong <2412160533@qq.com>
2022-05-30 20:24:44 +0800

    教务管理菜单

---
+++ src/views/EduMenus.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menus">
+    <AdminHeader />
+    <div class="main">
+      <EduSideMenus />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminHeader from "../components/AdminHeader";
+import EduSideMenus from "../components/EduSidemenus"
+
+export default {
+  name: "EduMenus",
+  components: {
+    AdminHeader,
+    EduSideMenus,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.menus{
+  background-color: #f3f5f7;
+  .main{
+    display: flex;
+    width: 1220px;
+    margin: 0 auto;
+  }
+}
+</style>
