a880d8c255f14558124087c315e80784764631d9
chehaisong <2412160533@qq.com>
2022-05-30 20:39:54 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,13 +5,34 @@ Vue.use(VueRouter);
 
 // 引入登录组件
 import Login from "../views/Login"
+// 引入忘记密码组件
 import ForgetPassword from "../views/ForgetPassword"
-import StudentIndex from "../views/StudentIndex"
-import Menus from "../views/Menus"
-import AdminIndex from "../views/AdminIndex"
+//引入管理员(系统管理)菜单
+import SystemMenus from "../views/SystemMenus"
+//引入管理员(系统管理->用户管理)组件
 import UserManage from "../views/UserManage"
+//引入管理员(教务管理)菜单
+import EduMenus from "../views/EduMenus"
+//引入管理员(教务管理->学生管理)组件
+import StudentManage from "../views/StudentManage"
+//引入管理员(教务管理->教师管理)组件
+import TeacherManage from "../views/TeacherManage"
+//引入管理员(教务管理->班级管理)组件
+import ClassManage from "../views/ClassManage"
 
 
+//引入管理员首页组件
+import AdminIndex from "../views/AdminIndex"
+//引入职位管理组件
+import PositionManage from "../views/PositionManage"
+//引入学生首页组件
+import StudentIndex from "../views/StudentIndex"
+//引入学生上方菜单
+import StudentMenus from "../views/StudentMenus"
+//引入学生课程组件
+import StudentCourse from "../views/StudentCourse"
+//引入学生考试测试组件
+import StudentTest from "../views/StudentTest"
 export default new VueRouter({
     routes: [
         {
@@ -27,7 +48,7 @@ export default new VueRouter({
         {
             path: "/student",
             name: "StudentIndex",
-            component: StudentIndex
+            component: StudentIndex,
         },
         {
             path: "/admin",
@@ -35,17 +56,64 @@ export default new VueRouter({
             component: AdminIndex
         },
         {
-            path:"/menus",
-            name: "Menus",
-            component: Menus,
+            path:"/systemmenus",
+            name: "SystemMenus",
+            component: SystemMenus,
             children:[
                 {
                     path:"usermanage",
                     name: "UserManage",
                     component: UserManage
+                },
+                {
+                    path:"positionmanage",
+                    name: "PositionManage",
+                    component: PositionManage
+                },
+            ]
+        },
+        {
+            path:"/edumenus",
+            name: "edumenus",
+            component: EduMenus,
+            children:[
+                {
+                    path:"studentmanage",
+                    name: "StudentManage",
+                    component: StudentManage
+                },
+                {
+                    path:"teachermanage",
+                    name: "TeacherManage",
+                    component: TeacherManage
+                },
+                {
+                    path:"classmanage",
+                    name: "ClassManage",
+                    component: ClassManage
                 }
             ]
         },
+
+        {
+            path:"/studentmenus",
+            name: "StudentMenus",
+            component: StudentMenus,
+            children:[
+                {
+                    path:"studentcourse",
+                    name: "StudentCourse",
+                    component: StudentCourse
+                },
+                {
+                    path:"studenttest",
+                    name: "StudentTest",
+                    component: StudentTest
+                },
+
+            ]
+        },
+        
     ]
 
 })
