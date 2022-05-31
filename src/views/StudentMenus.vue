525e48db206ee73ffe1243f8f35cdf895903abf1
chehaisong <2412160533@qq.com>
2022-05-31 21:28:52 +0800

    学生页面头部菜单

---
+++ src/views/StudentMenus.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menus">
+    <StudentHeader />
+    <div class="main">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import StudentHeader from "../components/StudentHeader";
+export default {
+  name: "StudentMenus",
+  components: {
+    StudentHeader,
+
+  },
+};
+</script>
+
+<style lang="less" scoped>
+// .menus{
+//   background-color: #f3f5f7;
+//   .main{
+//     display: flex;
+//     width: 1220px;
+//     margin: 0 auto;
+//   }
+// }
+</style>
