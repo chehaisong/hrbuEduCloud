139fd0e475e3e3e84f0fa50f70330b50581fecd6
chehaisong <2412160533@qq.com>
2022-05-31 21:27:30 +0800

    学生课程页

---
+++ src/views/StudentCourse.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>学生课程页面</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:"StudentCourse",
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
