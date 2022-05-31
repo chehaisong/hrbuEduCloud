29d2f8f4d0ce047a45f5fb62b731770d9ca53062
chehaisong <2412160533@qq.com>
2022-05-31 21:28:09 +0800

    学生考试测评页

---
+++ src/views/StudentTest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>学生测试页面</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name:"StudentTest",
+    components:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
