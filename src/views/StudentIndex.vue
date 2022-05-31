a6b51ca2e481831b3e66b3cc097c31e1943ad1a9
chehaisong <2412160533@qq.com>
2022-05-31 21:26:55 +0800

    学生首页

--- src/views/StudentIndex.vue
+++ src/views/StudentIndex.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-      学生端首页
+    <StudentHeader />
+    <h1>学生端首页</h1>
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
+import StudentHeader from "../components/StudentHeader.vue";
 export default {
-    name:"StudentIndex"
-}
+  name: "StudentIndex",
+  components: {
+    StudentHeader,
+  },
+};
 </script>
 
 <style>
-
 </style>
