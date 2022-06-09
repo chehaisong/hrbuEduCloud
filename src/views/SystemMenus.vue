5e239e6cb6f063cda551841e5fe05de49573e66e
chehaisong <2412160533@qq.com>
2022-06-09 22:22:54 +0800

    系统管理菜单

---
+++ src/views/SystemMenus.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="menus">
+    <AdminHeader />
+    <div class="main">
+      <SystemSidemenus />
+      <router-view></router-view>
+    </div>
+        <div>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminHeader from "../components/AdminHeader";
+import SystemSidemenus from "../components/SystemSidemenus";
+import Footer from "../components/Footer";
+export default {
+  name: "MyMenus",
+  components: {
+    AdminHeader,
+    SystemSidemenus,
+    Footer,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.menus{
+  background-color: #f3f5f7;
+  .main{
+    display: flex;
+    width: 1220px;
+    margin: 0 auto;
+  }
+}
+</style>
