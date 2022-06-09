3b587983da45d9f106abf0ac31d0d37bee6f6fef
chehaisong <2412160533@qq.com>
2022-06-09 22:09:12 +0800

    管理员个人中心菜单

---
+++ src/views/AdmincenterMenus.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="menus">
+    <AdminHeader />
+    <div class="main">
+      <AdmincenterSidemenus />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminHeader from "../components/AdminHeader";
+import AdmincenterSidemenus from "../components/AdmincenterSidemenus"
+
+export default {
+  name: "AdmincenterMenus",
+  components: {
+    AdminHeader,
+    AdmincenterSidemenus,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.menus{
+  background-color: #f3f5f7;
+  .main{
+    display: flex;
+    width: 1220px;
+    margin: 0 auto;
+  }
+}
+</style>
