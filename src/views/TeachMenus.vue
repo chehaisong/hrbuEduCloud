f64964922d16127f53d1aae9528ab1492c27b614
chehaisong <2412160533@qq.com>
2022-06-09 22:25:30 +0800

    教务管理菜单

---
+++ src/views/TeachMenus.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="menus">
+    <AdminHeader />
+    <div class="main">
+      <TeachmanageSidemenus />
+      <router-view></router-view>
+    </div>
+    <div>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminHeader from "../components/AdminHeader";
+import TeachmanageSidemenus from "../components/TeachmanageSidemenus";
+import Footer from "../components/Footer";
+
+export default {
+  name: "MyMenus",
+  components: {
+    AdminHeader,
+    TeachmanageSidemenus,
+    Footer,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.menus {
+  background-color: #f3f5f7;
+  .main {
+    display: flex;
+    width: 1220px;
+    margin: 0 auto;
+  }
+}
+</style>
